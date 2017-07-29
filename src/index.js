c4b66739b0524e87dd61a83c633abc7031c4c429
montuckymike <mike@kosmojo.com>
2017-07-29 10:24:48 -0700

    updated index.js - forgot to save this one before last commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,3 @@ import App from "./App";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import 'bootstrap/dist/css/bootstrap.css';
-// import './styles.css';
-// import AboutUs from "./components/AboutUs"
-// import Menu from "./components/Menu"
-// import NavBar from "./components/NavBar"
-//
-//
