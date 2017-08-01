fbef13266c68ef06718b9cf21797f7b748fed54f
montuckymike <mike@kosmojo.com>
2017-08-01 09:56:12 -0700

    playing with progress bar and currency convert

--- src/Menu/MenuApp.js
+++ src/Menu/MenuApp.js
@@ -2,7 +2,14 @@ import React from "react";
 import DATA from "./DATA.js";
 import 'bootstrap/dist/css/bootstrap.css';
 
+
+
+
 const MenuApp = () => {
+  const convertIntoCurrency = (N) => {N=parseFloat(N);if(!isNaN(N))N=N.toFixed(2);else N='0.00';return "$"+N;};
+  const convertToPercentage = (V) => {
+    return V / 10
+  }
   const menuItems = DATA.items.map(function(item){
   return(
       <div className="menu-item">
@@ -10,8 +17,18 @@ const MenuApp = () => {
           <img src={ item.img } />
           <div className="caption">
             <h3> { item.name } </h3>
-            <p> { item.price } </p>
+            <p> <strong>Price: {convertIntoCurrency(item.price)}</strong></p>
+
+            <div className="progress">
+              <div className="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow= {item.spiceLevel} aria-valuemin="0" aria-valuemax="100" style={{"width": convertToPercentage(item.spiceLevel) }}>
+                <span className="sr-only"> Spice Level </span>
+              </div>
+            </div>
+
+
             <p> { item.spiceLevel } </p>
+
+
             <p> { item.category } </p>
             <p> <a href="#" className="btn btn-primary" role="button">Order</a></p>
           </div>
