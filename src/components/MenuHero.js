783ccea87f7ebc6a26928b63d7d3ff3228804284
montuckymike <mike@kosmojo.com>
2017-08-03 09:10:11 -0700

    updated CSS

--- src/components/MenuHero.js
+++ src/components/MenuHero.js
@@ -1,23 +1,15 @@
 import React from "react";
+import "./MenuItems.css";
 
 const MenuHero = () => {
-  const hero = () => {
     return (
-      <div>
+      <div className="hero-image">
         <div className="hero-text">
           <h1 className="hero-title"> Tex-Mex </h1>
           <button> Contact Us </button>
         </div>
       </div>
     )
-  }
-  return (
-    <div>
-      <div className="hero-image">
-        { hero }
-      </div>
-    </div>
-  )
 }
 
 
